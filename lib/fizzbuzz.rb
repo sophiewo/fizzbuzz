849c407f3c507af0ad872d2a84be0cd15c65b54f
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-01-16 15:13:02 +0000

    Commit to share with Dafna

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,19 @@
 def fizzbuzz(number)
-  'fizz'
+    if number % 3 == 0 && number % 5 == 0
+      return 'fizzbuzz'
+    elsif number % 3 == 0
+      return 'fizz'
+    elsif number % 5 == 0
+      return 'buzz'
+    else
+      return number
+    end
 end
+
+
+number = 0
+
+while number < 101
+  puts fizzbuzz number
+  number += 1
+  end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,18 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+end
